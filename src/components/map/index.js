8de98940a9dbd1bec44243e10232aeb7eb690183
R1klus <rikhardur.fridgeirsson@gmail.com>
2022-06-25 00:25:44 +0000

    Map implemented with center on Iceland and working drag, zoom and pitch controls

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,16 +1,37 @@
 import React, {useRef, useEffect, useState} from "react";
-import {useSelector, useDispatch} from "react-redux";
-import {getEarthquakeData} from "../../redux/actions";
+import {useSelector} from "react-redux";
+import ReactMapGL, {Marker} from "react-map-gl"
+import styles from "./styles.scss"
 
 
 const EarthquakeMap = () => {
-    const earthquakeData = useSelector(({earthquakeData}) => earthquakeData);
+    const earthquakes = useSelector(({earthquakes}) => earthquakes);
 
+    useEffect(() => {
+        console.log(earthquakes)
+        console.log(process.env.REACT_APP_MAPBOX_API_KEY)
+        console.log(process.env.REACT_APP_EARTHQUAKE_API_URL)
+    }, [earthquakes])
+
+    const [viewport, setViewport] = useState({
+        latitude: 64.8085175,
+        longitude: -18.806046007266094,
+        width: "95%",
+        height: "95%",
+        zoom: 7,
+    })
 
     return (
-        <div>
-            Hello World!
+        <div className="map-container">
+            <ReactMapGL
+                {...viewport}
+                mapboxAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
+                mapStyle="mapbox://styles/mapbox/dark-v10"
+                onMove={evt => setViewport(evt.viewState)}
+            >
+            </ReactMapGL>
         </div>
+
     )
 }
 
