7fbae80df802023deb1adf5981f9cfd33bf590f6
R1klus <rikhardur.fridgeirsson@gmail.com>
2022-06-25 16:25:45 +0000

    Added navbar with mobile support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './styles/index.scss';
 import {Routes, Route} from 'react-router-dom';
-import Homepage from "./views/homepage";
+import MapView from "./views/mapView";
+import DataView from "./views/dataView";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Homepage/>}/>
+        <Route exact path="/" element={<MapView/>}/>
+        <Route exact path="/data" element={<DataView />}/>
     </Routes>
   );
 }
