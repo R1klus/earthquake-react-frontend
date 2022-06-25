bd404ca8a32323898ef40bc684f3a76297d6a2fa
R1klus <rikhardur.fridgeirsson@gmail.com>
2022-06-25 17:25:59 +0000

    added toggle switch for light and darkmode to navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import './styles/index.scss';
 import {Routes, Route} from 'react-router-dom';
 import MapView from "./views/mapView";
 import DataView from "./views/dataView";
+import {createContext, useEffect, useState} from "react";
+
+export const themeContext = createContext(null);
 
 function App() {
-  return (
-    <Routes>
-        <Route exact path="/" element={<MapView/>}/>
-        <Route exact path="/data" element={<DataView />}/>
-    </Routes>
-  );
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = () => {
+        setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+    }
+
+    useEffect(() => {
+        console.log(theme)
+    })
+
+    return (
+        <themeContext.Provider value={{
+            currentTheme: theme,
+            toggleTheme: toggleTheme}}>
+            <div className="app" id={theme}>
+                <Routes>
+                    <Route exact path="/" element={<MapView/>}/>
+                    <Route exact path="/data" element={<DataView/>}/>
+                </Routes>
+            </div>
+        </themeContext.Provider>
+    );
 }
 
 export default App;
