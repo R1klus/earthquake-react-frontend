fb547d1fab41ab4d057c7fc7b1f411a4d04bed7f
R1klus <rikhardur.fridgeirsson@gmail.com>
2022-06-26 21:54:20 +0000

    Sorting by column for all columns added

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,9 +1,28 @@
-import {FETCH_FROM_API, GET_EARTHQUAKE_DATA, NEW_FETCH_FREQUENCY} from "../constants";
+import {
+    FETCH_FROM_API,
+    GET_EARTHQUAKE_DATA,
+    NEW_FETCH_FREQUENCY,
+    REVERSE_EARTHQUAKE_DATA,
+    SORT_EARTHQUAKE_DATA
+} from "../constants";
 
 export const getEarthquakeData = (earthquakeData) => {
     return {
         type: GET_EARTHQUAKE_DATA,
         payload: earthquakeData
+    }
+}
+
+export const sortEarthquakeData = (sortingFunction) => {
+    return {
+        type: SORT_EARTHQUAKE_DATA,
+        payload: sortingFunction
+    }
+}
+
+export const reverseEarthquakeData = () => {
+    return {
+        type: REVERSE_EARTHQUAKE_DATA,
     }
 }
 
